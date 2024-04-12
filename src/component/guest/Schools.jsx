5ae08d30dafda3cc65c54a4b298593532138cc43
Regis JOUAN <regis.jouan@lapiscine.pro>
2024-04-12 16:07:20 +0200

    page accueil + footer + header

--- src/component/guest/Schools.jsx
+++ src/component/guest/Schools.jsx
@@ -2,30 +2,32 @@ import "./Schools.scss";
 
 function Schools () {
     return (
-    <section id="https://http://localhost:3001/#lienchools">
-        <h2>Les établissement scolaires</h2>
+    <section id="http://localhost:3001/#lienchools">
+        <div class="soustitre">
+            <h2>Les établissements scolaires</h2>
+        </div>
         <div class="container-schools" >            
             <div class="container-school">
                 <a href="http://localhost:3001/maternelle">
-                    <div class="img-container-school"><img class="img-container-school" src="../../assets/imgs/maternelle.jpg" alt="la maternelle" /></div>
+                    <div class="img-container-school"><img class="img-school" src="../../assets/imgs/maternelle.jpg" alt="la maternelle" /></div>
                 </a> 
                     <h3>MATERNELLE</h3>
             </div>               
             <div class="container-school">
                 <a href="#">
-                    <div class="img-container-school"><img class="img-container-school" src="../../assets/imgs/elementaire.png" alt="l'école élementaire" /></div>
+                    <div class="img-container-school"><img class="img-school" src="../../assets/imgs/elementaire.png" alt="l'école élementaire" /></div>
                 </a>
                 <h3>ELEMENTAIRE</h3>
             </div>  
             <div class="container-school">
             <a href="#">
-                <div class="img-container-school"><img class="img-container-school" src="../../assets/imgs/college.jpg" alt="le collége" /></div>
+                <div class="img-container-school"><img class="img-school" src="../../assets/imgs/college.jpg" alt="le collége" /></div>
                 </a>   
                 <h3>COLLEGE</h3>
             </div>  
             <div class="container-school">
             <a href="#">
-                <div class="img-container-school"><img class="img-container-school" src="../../assets/imgs/lycée.jpg" alt="le lycée" /></div>
+                <div class="img-container-school"><img class="img-school" src="../../assets/imgs/lycée.jpg" alt="le lycée" /></div>
                 </a>   
                 <h3>LYCEE</h3>
             </div>    
